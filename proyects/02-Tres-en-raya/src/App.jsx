460176e04e15c27042bb457dc1946971f2756594
JSampereVázquez <72076376+jsamperevazquez@users.noreply.github.com>
2023-06-21 09:56:17 +0200

    Creados métodos comprobar ganador

--- proyects/02-Tres-en-raya/src/App.jsx
+++ proyects/02-Tres-en-raya/src/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import { useState } from "react";
 import "./App.css";
 
@@ -9,23 +10,67 @@ const TURNS = {
 const Square = ({ children, updateBoard, isSelected, index }) => {
   const className = `square ${isSelected ? "is-selected" : ""}`;
 
-  const handleClick = () =>{
-    updateBoard(index)
-  }
+  const handleClick = () => {
+    updateBoard(index);
+  };
 
-  return <div onClick={handleClick} className={className}>{children}</div>;
+  return (
+    <div onClick={handleClick} className={className}>
+      {children}
+    </div>
+  );
 };
+const WINNER_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null));
   const [turn, setTurn] = useState(TURNS.X);
-  
+  const [winner, setWinner] = useState(null); //null es no hay ganador, false que hay empate
+
+  const checkWinner = (boardToCheck) => {
+    // Revisamos las posibles combinaciones ganadoras
+    for (const combo of WINNER_COMBOS) {
+      const [a, b, c] = combo;
+      if (
+        boardToCheck[a] &&
+        boardToCheck[a] === boardToCheck[b] &&
+        boardToCheck[a] === boardToCheck[c]
+      ) {
+        return boardToCheck[a];
+      }
+    }
+    // si no hay ganador
+    return null;
+  };
+
   const updateBoard = (index) => {
-    const newBoard = [...board]
-    newBoard[index] = turn
-    setBoard(newBoard)
-    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
-    setTurn(newTurn)
-  }
+    // No actualizamos la posición si ya tiene algo:
+    if (board[index] || winner) return;
+    // Si es null actualizamos el tablero:
+    const newBoard = [...board];
+    newBoard[index] = turn;
+    setBoard(newBoard);
+
+    //Cambiar el turno:
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
+    setTurn(newTurn);
+
+    //Revisamos si hay ganador
+    const newWinner = checkWinner(newBoard);
+    if (newWinner) {
+      setWinner(newWinner);
+      alert(`El ganador es ${newWinner}`);
+    }
+  };
   return (
     <main className="board">
       <h1>Tres en raya</h1>
